af2eb63975c990d75ecea1d824dea3404bb97223
6w5zp78jzg-ux <6w5zp78jzg@privaterelay.appleid.com>
2026-05-02 21:51:20 +0000

    knlkm

--- million-dollars-listing/next.config.ts
+++ million-dollars-listing/next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  reactStrictMode: false,
   serverExternalPackages: ["next-intl"],
   images: {
     formats: ["image/avif", "image/webp"],
