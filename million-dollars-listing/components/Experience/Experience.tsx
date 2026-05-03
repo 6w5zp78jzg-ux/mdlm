fb342df47354c40ce7504f2c23a15210bf449660
6w5zp78jzg-ux <6w5zp78jzg@privaterelay.appleid.com>
2026-05-03 11:36:35 +0000

    n

--- million-dollars-listing/components/Experience/Experience.tsx
+++ million-dollars-listing/components/Experience/Experience.tsx
@@ -31,7 +31,7 @@ export default function Experience() {
           infographic1Ref={infographic1Ref}
           infographic2Ref={infographic2Ref}
         />
-        <GallerySection galleryTrackRef={galleryTrackRef} />
+        <GallerySection galleryTrackRef={galleryTrackRef} images={["/gallery/228e370e-3d41-406d-a6aa-d72bc8cc2772.jpeg","/gallery/a741c346-7225-4ccb-81fb-13b02e7f5573.jpeg","/gallery/a8c5ab21-42e6-4e78-8613-43737facb1d8.jpeg"]} />
       </div>
     </div>
   );
